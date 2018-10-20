100ead92ee54f71b62255f2cb76468859e44775e
Emil Lundberg <emil@emlun.se>
2018-10-20 13:56:34 +0200

    Make release.sh also sign the standalone script

--- release.sh
+++ release.sh
@@ -5,6 +5,9 @@ set -e # Exit on error
 
 version="${1:-$(./compute-version.sh)}"
 
+SCRIPT_SRC_FILENAME=yubikey-oath-dmenu
+SCRIPT_VERSIONED_FILENAME="${SCRIPT_SRC_FILENAME}-${version}.py"
+SCRIPT_SIG_FILENAME="${SCRIPT_VERSIONED_FILENAME}.sig"
 PROJECT_NAME=yubikey-oath-dmenu
 TAR_FILE="${PROJECT_NAME}"-"${version}".tar.gz
 SIG_FILE="${TAR_FILE}".sig
@@ -13,6 +16,11 @@ TAG=v"${version}"
 
 git archive --prefix "${PREFIX}" -o "${TAR_FILE}" "${TAG}"
 gpg --detach-sign "${TAR_FILE}"
+gpg --verify "${SIG_FILE}"
+
+
+install -m 644 "${SCRIPT_SRC_FILENAME}" "${SCRIPT_VERSIONED_FILENAME}"
+gpg --detach-sign "${SCRIPT_VERSIONED_FILENAME}"
 gpg --verify "${SIG_FILE}"
 
 echo "Successfully released version ${version}"
